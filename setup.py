f391a8a1b8677dbca4a6c4f7f073c025f8d29990
Khulan <khulan.ulz@gmail.com>
2023-05-27 18:29:27 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sample',
+    version='0.0.0',
+    description='Rule out Red Blood Cell Antibody from Antibody Panels/Antigrams for Blood Bank laboratory',
+    long_description=readme,
+    author='Khulan Ulziibat',
+    author_email='khulan.ulz@gmail.com',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
