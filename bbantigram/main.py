e004670b65a692a811e847ac450c557bab645902
Khulan <khulan.ulz@gmail.com>
2023-05-28 00:23:20 -0400

    implemented ruling out

--- bbantigram/main.py
+++ bbantigram/main.py
@@ -3,7 +3,7 @@ from copy import deepcopy
 import pandas as pd
  
 #constants
-DOUBLE_DOSE = ['C', 'E', 'c', 'e', 'Fya', 'Fyb', 'Jka', 'Jkb', 'M', 'N', 'S', 's']
+DOUBLE_DOSE = [('C', 'c'), ('E', 'e'), ('Fya', 'Fyb'), ('Jka', 'Jkb'), ('M', 'N'), ('S', 's')]
 SCREEN_CELL_SIZE = 3
 PANEL_SIZE = 11
 ANTIGENS = ['D', 'C', 'E', 'c', 'e', 'f', 'Cw', 'V', 'K', 'k', 'Kpa', 'Kpb', 'Jsa', 'Jsb', 'Fya', 'Fyb', 'Jka', 'Jkb', 'P1', 'Lua', 'Lub', 'M', 'N', 'S', 's', 'Lea', 'Leb']
@@ -16,22 +16,57 @@ panel = [[0] * len(ANTIGENS)] * PANEL_SIZE
 panel_results = ['3+', '3+', '0', '0', '0', '1+', '0', '1+', '3+', '1+', '0']
 autocontrol = None
 
+# checks if antigen is heterozygous or homozygous
+def is_double_dose(antigen, row):
+    for pair in DOUBLE_DOSE:
+        if antigen in pair:
+            if pair[0] == antigen:
+                other = pair[1]
+            else:
+                other = pair[1]
+            break
+    i1 = ANTIGENS.index[antigen]
+    i2 = ANTIGENS.index[other]
+
+    global panel
+    if panel[row][i1] == '+' and panel[row][i2] == '+':
+        return True
+    return False
+    
+        
+
 def initialize_panel():
     df = pd.read_excel("Panel1.xlsx")
     print(df)
+    global panel 
     panel = [[str(x) for x in l] for l in df.to_numpy()]
     print(panel)
 
 def rule_out():
+    # for each donor cell
     for i in range(PANEL_SIZE):
+        # only check for antigens to rule out for cells with negative reaction
         if panel_results[i] == '0':
-            return
+            # for each antigen
+            for j in range(len(ANTIGENS)):
+                antigen = ANTIGENS[j]
+                # only consider antigens not ruled out yet and not present in double dose 
+                if antigen in ab_to_eliminate and (antigen not in DOUBLE_DOSE or not is_double_dose(antigen, i)):
+                    if panel[i][j] == '+':
+                        cross_count[antigen] += 1
+                        if cross_count[antigen] >= 2:
+                            ab_to_eliminate.remove(antigen)
+    print(cross_count)
+    print(ab_to_eliminate)
+
 
 def find_best_fit():
     return
 
 def main():
     initialize_panel()
+    print(panel)
+    rule_out()
     return 
 
 if __name__ == "__main__":
